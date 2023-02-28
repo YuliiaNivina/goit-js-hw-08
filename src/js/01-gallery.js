a54c2804aee34e77d536ae5c781c80de7af98ab4
Yuliia Nivina <y.nivina@gmail.com>
2023-02-28 22:12:44 +0200

    Add task-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -21,15 +21,13 @@ function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-      <li>
         <a class="gallery__item" href="${original}">
            <img
            class="gallery__image"
            src="${preview}"
            alt="${description}"
            />
-        </a>
-      </li>`;
+        </a>`;
     })
     .join("");
 }
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,52 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  const playTime = data;
+
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(playTime));
+
+  // console.log(playTime);
+}
+
+function playOnSavedTime() {
+  const getTime = localStorage.getItem('videoplayer-current-time');
+  const parsedGetTime = JSON.parse(getTime);
+  // console.log(getTime);
+  // console.log(parsedGetTime);
+
+  const { seconds } = parsedGetTime;
+  // console.log(seconds);
+
+  player
+    .setCurrentTime(seconds)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
+playOnSavedTime();
